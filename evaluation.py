4a0717518f4864ec54faea1072af816bbba7dc38
Claudia Crespo <claudy991@msn.com>
2021-12-26 22:57:58 -0500

    initial commit

---
+++ evaluation.py
@@ -0,0 +1,376 @@
+"""Evaluation"""
+
+from __future__ import print_function
+from PIL import *
+import sys
+import time
+import torch
+import numpy as np
+from data import get_test_loader
+from vocab import deserialize_vocab
+from model import SGRAF
+from collections import OrderedDict
+
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=0):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / (.0001 + self.count)
+
+    def __str__(self):
+        """String representation for logging
+        """
+        # for values that should be recorded exactly e.g. iteration number
+        if self.count == 0:
+            return str(self.val)
+        # for stats
+        return '%.4f (%.4f)' % (self.val, self.avg)
+
+
+class LogCollector(object):
+    """A collection of logging objects that can change from train to val"""
+
+    def __init__(self):
+        # to keep the order of logged variables deterministic
+        self.meters = OrderedDict()
+
+    def update(self, k, v, n=0):
+        # create a new meter if previously not recorded
+        if k not in self.meters:
+            self.meters[k] = AverageMeter()
+        self.meters[k].update(v, n)
+
+    def __str__(self):
+        """Concatenate the meters in one log line
+        """
+        s = ''
+        for i, (k, v) in enumerate(self.meters.items()):
+            if i > 0:
+                s += '  '
+            s += k + ' ' + str(v)
+        return s
+
+    def tb_log(self, tb_logger, prefix='', step=None):
+        """Log using tensorboard
+        """
+        for k, v in self.meters.items():
+            tb_logger.log_value(prefix + k, v.val, step=step)
+
+
+def encode_data(model, data_loader, log_step=10, logging=print):
+    """Encode all images and captions loadable by `data_loader`
+    """
+    val_logger = LogCollector()
+
+    # switch to evaluate mode
+    model.val_start()
+
+    # np array to keep all the embeddings
+    img_embs = None
+    cap_embs = None
+
+    max_n_word = 0
+    for i, (images, captions, lengths, ids) in enumerate(data_loader):
+        max_n_word = max(max_n_word, max(lengths))
+
+    for i, (images, captions, lengths, ids) in enumerate(data_loader):
+        # make sure val logger is used
+        model.logger = val_logger
+
+        # compute the embeddings
+        with torch.no_grad():
+            img_emb, cap_emb, cap_len = model.forward_emb(images, captions, lengths)
+        if img_embs is None:
+            img_embs = np.zeros((len(data_loader.dataset), img_emb.size(1), img_emb.size(2)))
+            cap_embs = np.zeros((len(data_loader.dataset), max_n_word, cap_emb.size(2)))
+            cap_lens = [0] * len(data_loader.dataset)
+        # cache embeddings
+        img_embs[ids] = img_emb.data.cpu().numpy().copy()
+        cap_embs[ids, :max(lengths), :] = cap_emb.data.cpu().numpy().copy()
+
+        for j, nid in enumerate(ids):
+            cap_lens[nid] = cap_len[j]
+
+        del images, captions
+    return img_embs, cap_embs, cap_lens
+
+
+def evalrank(model_path, data_path=None, split='dev', fold5=False):
+    """
+    Evaluate a trained model on either dev or test. If `fold5=True`, 5 fold
+    cross-validation is done (only for MSCOCO). Otherwise, the full data is
+    used for evaluation.
+    """
+    # load model and options
+    checkpoint = torch.load(model_path)
+    opt = checkpoint['opt']
+    save_epoch = checkpoint['epoch']
+    print(opt)
+    if data_path is not None:
+        opt.data_path = data_path
+
+    # load vocabulary used by the model
+    vocab = deserialize_vocab('./vocab/%s_vocab.json' % opt.data_name)
+    opt.vocab_size = len(vocab)
+
+    # construct model
+    model = SGRAF(opt)
+
+    # load model state
+    model.load_state_dict(checkpoint['model'])
+
+    print('Loading dataset')
+    data_loader = get_test_loader(split, opt.data_name, vocab,
+                                  opt.batch_size, opt.workers, opt)
+    print("=> loaded checkpoint_epoch {}".format(save_epoch))
+
+    print('Computing results...')
+    img_embs, cap_embs, cap_lens = encode_data(model, data_loader)
+    print('Images: %d, Captions: %d' %
+    (img_embs.shape[0] / 5, cap_embs.shape[0]))
+    
+ 
+    # w, h = 512, 512
+    # img = Image.fromarray(data_loader.dataset.images[0], 'RGB')
+    # img.save('my.png')
+    # img.show()
+
+    # print(data_loader.dataset.captions[0])
+
+    # plt.show()
+    if not fold5:
+        # no cross-validation, full evaluation
+        img_embs = np.array([img_embs[i] for i in range(0, len(img_embs), 5)])
+
+        # record computation time of validation
+        start = time.time()
+        # caption = captions(model, img_embs, cap_embs, cap_lens, data_loader,vocab, shard_size=100 )
+        sims = shard_attn_scores(model, img_embs, cap_embs, cap_lens, opt, shard_size=100)
+        end = time.time()
+        print("calculate similarity time:", end-start)
+
+        # bi-directional retrieval
+        r, rt = i2t(img_embs, cap_embs, cap_lens, sims, return_ranks=True)
+        ri, rti = t2i(img_embs, cap_embs, cap_lens, sims, return_ranks=True)
+        ar = (r[0] + r[1] + r[2]) / 3
+        ari = (ri[0] + ri[1] + ri[2]) / 3
+        rsum = r[0] + r[1] + r[2] + ri[0] + ri[1] + ri[2]
+        print("rsum: %.1f" % rsum)
+        print("Average i2t Recall: %.1f" % ar)
+        print("Image to text: %.1f %.1f %.1f %.1f %.1f" % r)
+        print("Average t2i Recall: %.1f" % ari)
+        print("Text to image: %.1f %.1f %.1f %.1f %.1f" % ri)
+    else:
+        # 5fold cross-validation, only for MSCOCO
+        results = []
+        for i in range(5):
+            img_embs_shard = img_embs[i * 5000:(i + 1) * 5000:5]
+            cap_embs_shard = cap_embs[i * 5000:(i + 1) * 5000]
+            cap_lens_shard = cap_lens[i * 5000:(i + 1) * 5000]
+
+            start = time.time()
+            
+
+            sims = shard_attn_scores(model, img_embs_shard, cap_embs_shard, cap_lens_shard, opt, shard_size=100)
+            end = time.time()
+            print("calculate similarity time:", end-start)
+
+            r, rt0 = i2t(img_embs_shard, cap_embs_shard, cap_lens_shard, sims, return_ranks=True)
+            
+      
+            print("Image to text: %.1f, %.1f, %.1f, %.1f, %.1f" % r)
+            ri, rti0 = t2i(img_embs_shard, cap_embs_shard, cap_lens_shard, sims, return_ranks=True)
+            print("Text to image: %.1f, %.1f, %.1f, %.1f, %.1f" % ri)
+
+            if i == 0:
+                rt, rti = rt0, rti0
+            ar = (r[0] + r[1] + r[2]) / 3
+            ari = (ri[0] + ri[1] + ri[2]) / 3
+            rsum = r[0] + r[1] + r[2] + ri[0] + ri[1] + ri[2]
+            print("rsum: %.1f ar: %.1f ari: %.1f" % (rsum, ar, ari))
+            results += [list(r) + list(ri) + [ar, ari, rsum]]
+
+        print("-----------------------------------")
+        print("Mean metrics: ")
+        mean_metrics = tuple(np.array(results).mean(axis=0).flatten())
+        print("rsum: %.1f" % (mean_metrics[10] * 6))
+        print("Average i2t Recall: %.1f" % mean_metrics[11])
+        print("Image to text: %.1f %.1f %.1f %.1f %.1f" %
+              mean_metrics[:5])
+        print("Average t2i Recall: %.1f" % mean_metrics[12])
+        print("Text to image: %.1f %.1f %.1f %.1f %.1f" %
+              mean_metrics[5:10])
+
+
+def shard_attn_scores(model, img_embs, cap_embs, cap_lens, opt, shard_size=100):
+    n_im_shard = (len(img_embs) - 1) // shard_size + 1
+    n_cap_shard = (len(cap_embs) - 1) // shard_size + 1
+
+    sims = np.zeros((len(img_embs), len(cap_embs)))
+    for i in range(n_im_shard):
+        im_start, im_end = shard_size * i, min(shard_size * (i + 1), len(img_embs))
+        for j in range(n_cap_shard):
+            sys.stdout.write('\r>> shard_attn_scores batch (%d,%d)' % (i, j))
+            ca_start, ca_end = shard_size * j, min(shard_size * (j + 1), len(cap_embs))
+
+            with torch.no_grad():
+                im = torch.from_numpy(img_embs[im_start:im_end]).float().cuda()
+                ca = torch.from_numpy(cap_embs[ca_start:ca_end]).float().cuda()
+                l = cap_lens[ca_start:ca_end]
+                sim = model.forward_sim(im, ca, l)
+                    
+            sims[im_start:im_end, ca_start:ca_end] = sim.data.cpu().numpy()
+    sys.stdout.write('\n')
+    return sims
+
+from model import SCAN_attention
+def captions(model, img_embs, cap_embs, cap_lens, data_loader,vocab, shard_size=100):
+    n_im_shard = (len(img_embs) - 1) // shard_size + 1
+    n_cap_shard = (len(cap_embs) - 1) // shard_size + 1
+
+    sims = np.zeros((len(img_embs), len(cap_embs)))
+    for i in range(n_im_shard):
+        im_start, im_end = shard_size * i, min(shard_size * (i + 1), len(img_embs))
+        for j in range(n_cap_shard):
+            sys.stdout.write('\r>> shard_attn_scores batch (%d,%d)' % (i, j))
+            ca_start, ca_end = shard_size * j, min(shard_size * (j + 1), len(cap_embs))
+
+            with torch.no_grad():
+                im = torch.from_numpy(img_embs[im_start:im_end]).float().cuda()
+                ca = torch.from_numpy(cap_embs[ca_start:ca_end]).float().cuda()
+                l = cap_lens[ca_start:ca_end]
+                # sim = model.forward_emb(im, ca, l)
+
+            weight = SCAN_attention(ca, im, smooth= 9.0, eps=1e-8)
+            visualize_att("./data/test_data/10002456.jpg", data_loader.dataset.captions[j], weight, vocab, smooth=True)
+            print()
+            sys.stdout.write('\n')
+
+def i2t(images, captions, caplens, sims, npts=None, return_ranks=False):
+    """
+    Images->Text (Image Annotation)
+    Images: (N, n_region, d) matrix of images
+    Captions: (5N, max_n_word, d) matrix of captions
+    CapLens: (5N) array of caption lengths
+    sims: (N, 5N) matrix of similarity im-cap
+    """
+    npts = images.shape[0]
+    ranks = np.zeros(npts)
+    top1 = np.zeros(npts)
+
+    for index in range(npts):
+        inds = np.argsort(sims[index])[::-1]
+
+        # Score
+        rank = 1e20
+        for i in range(5 * index, 5 * index + 5, 1):
+            tmp = np.where(inds == i)[0][0]
+            if tmp < rank:
+                rank = tmp
+        ranks[index] = rank
+        top1[index] = inds[0]
+
+    # Compute metrics
+    r1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    r5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    r10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+    medr = np.floor(np.median(ranks)) + 1
+    meanr = ranks.mean() + 1
+    if return_ranks:
+        return (r1, r5, r10, medr, meanr), (ranks, top1)
+    else:
+        return (r1, r5, r10, medr, meanr)
+
+
+def t2i(images, captions, caplens, sims, npts=None, return_ranks=False):
+    """
+    Text->Images (Image Search)
+    Images: (N, n_region, d) matrix of images
+    Captions: (5N, max_n_word, d) matrix of captions
+    CapLens: (5N) array of caption lengths
+    sims: (N, 5N) matrix of similarity im-cap
+    """
+    npts = images.shape[0]
+    ranks = np.zeros(5 * npts)
+    top1 = np.zeros(5 * npts)
+
+    # --> (5N(caption), N(image))
+    sims = sims.T
+
+    for index in range(npts):
+        for i in range(5):
+            inds = np.argsort(sims[5 * index + i])[::-1]
+            ranks[5 * index + i] = np.where(inds == index)[0][0]
+            top1[5 * index + i] = inds[0]
+
+    # Compute metrics
+    r1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    r5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    r10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+    medr = np.floor(np.median(ranks)) + 1
+    meanr = ranks.mean() + 1
+    if return_ranks:
+        return (r1, r5, r10, medr, meanr), (ranks, top1)
+    else:
+        return (r1, r5, r10, medr, meanr)
+
+import matplotlib.pyplot as plt
+from PIL import Image
+import skimage 
+
+def visualize_att(image_path, seq, alphas, rev_word_map, smooth=True):
+    """
+    Visualizes caption with weights at every word.
+
+    Adapted from paper authors' repo: https://github.com/kelvinxu/arctic-captions/blob/master/alpha_visualization.ipynb
+
+    :param image_path: path to image that has been captioned
+    :param seq: caption
+    :param alphas: weights
+    :param rev_word_map: reverse word mapping, i.e. ix2word
+    :param smooth: smooth weights?
+    """
+    image = Image.open(image_path)
+    image = image.resize([14 * 24, 14 * 24], Image.LANCZOS)
+
+    words = [rev_word_map[ind] for ind in seq] 
+
+    for t in range(len(words)):
+        if t > 50:
+            break
+        plt.subplot(np.ceil(len(words) / 5.), 5, t + 1)
+
+        plt.text(0, 1, '%s' % (words[t]), color='black', backgroundcolor='white', fontsize=12)
+        plt.imshow(image)
+        current_alpha = alphas[t, :]
+        if smooth:
+            alpha = skimage.transform.pyramid_expand(current_alpha.numpy(), upscale=24, sigma=8)
+        else:
+            alpha = skimage.transform.resize(current_alpha.numpy(), [14 * 24, 14 * 24])
+        if t == 0:
+            plt.imshow(alpha, alpha=0)
+        else:
+            plt.imshow(alpha, alpha=0.8)
+        plt.set_cmap(cm.Greys_r)
+        plt.axis('off')
+    plt.show()
+
+if __name__ == '__main__':
+    # predict("./runs/model_best.pth.tar",
+            #  split="test", query = "dog running",image_path= './data/test_data/10002456.jpg' )
+    evalrank("./runs/test runs/checkpoint_79.pth.tar",
+             data_path='./data', split="test", fold5=False)
---
+++ vocab.py
@@ -0,0 +1,157 @@
+# -----------------------------------------------------------
+# Stacked Cross Attention Network implementation based on 
+# https://arxiv.org/abs/1803.08024.
+# "Stacked Cross Attention for Image-Text Matching"
+# Kuang-Huei Lee, Xi Chen, Gang Hua, Houdong Hu, Xiaodong He
+#
+# Writen by Kuang-Huei Lee, 2018
+# ---------------------------------------------------------------
+"""Vocabulary wrapper"""
+
+from collections import Counter
+import argparse
+import os
+import json
+from numpy.core.fromnumeric import shape
+import gensim
+import en_core_web_sm
+
+annotations = {
+    'coco_precomp': ['train_caps.txt', 'dev_caps.txt'],
+    'f30k_precomp': ['train_caps.txt', 'dev_caps.txt'],
+}
+
+
+class Vocabulary(object):
+    """Simple vocabulary wrapper."""
+
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+
+    def add_word(self, word):
+        if word not in self.word2idx:
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
+
+    def __call__(self, word):
+        if word not in self.word2idx:
+            return self.word2idx['<unk>']
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.word2idx)
+
+
+def serialize_vocab(vocab, dest):
+    d = {}
+    d['word2idx'] = vocab.word2idx
+    d['idx2word'] = vocab.idx2word
+    d['idx'] = vocab.idx
+    with open(dest, "w") as f:
+        json.dump(d, f)
+
+
+def deserialize_vocab(src):
+    with open(src) as f:
+        d = json.load(f)
+    vocab = Vocabulary()
+    vocab.word2idx = d['word2idx']
+    vocab.idx2word = d['idx2word']
+    vocab.idx = d['idx']
+    return vocab
+
+
+def from_txt(txt):
+    captions = []
+    with open(txt, 'rb') as f:
+        for line in f:
+            line.decode('utf-8')
+            captions.append(line.strip())
+    return captions
+
+def build_vocab(data_path, data_name, caption_file, threshold):
+    nlp = en_core_web_sm.load()
+    doc = []
+    for path in caption_file[data_name]:
+        full_path = os.path.join(os.path.join(data_path, data_name), path)
+        text = from_txt(full_path)
+       
+        for item in text:
+            doc.append(nlp(item.decode('utf-8')))
+           
+        tokens = []        
+        for i, item in enumerate(doc):
+            for token in item:
+                print(token)
+                if token.lemma_ == '-PRON-':
+                    token.lemma_ = token.orth_ 
+                    token.lemma = token.orth 
+                if token not in nlp.Defaults.stop_words:
+                    tokens.append(token.lemma_ )
+                if i % 1000 == 0:
+                    print("[%d/%d] tokenized the captions." % (i, len(token)))
+                
+    vocab = Vocabulary()
+    vocab.add_word('<pad>')
+    vocab.add_word('<start>')
+    vocab.add_word('<end>')
+    vocab.add_word('<unk>')
+
+    # Add words to the vocabulary.
+    for i, word in enumerate(tokens):
+        vocab.add_word(word)
+    return vocab
+    
+   
+def word2vec(data_path,data_name,caption_file):
+    doc = []
+    tokens = []
+    nlp = en_core_web_sm.load()
+    stop_words= nlp.Defaults.stop_words 
+    for path in caption_file[data_name]:
+        full_path = os.path.join(os.path.join(data_path, data_name), path)
+        captions = from_txt(full_path)
+
+        for item in captions:
+            doc.append(gensim.utils.simple_preprocess(item))
+           
+        for i, token in enumerate(doc):
+            stopped_tokens = [i for i in token if not i in stop_words]
+            tokens.append(" ".join(stopped_tokens))
+            if i % 1000 == 0:
+                print("[%d/%d] tokenized the captions." % (i, len(doc)))
+                
+    documents = [_text.split() for _text in tokens] 
+    w2v_model = gensim.models.word2vec.Word2Vec(vector_size=300, 
+                                            window=7, 
+                                            min_count=10, 
+                                            workers=8,)    
+    w2v_model.build_vocab(documents)
+    words = w2v_model.wv.key_to_index.keys()
+    vocab_size = len(words)
+    print("Vocab size", vocab_size)
+    w2v_model.train(documents, total_examples=len(documents), epochs=32) 
+    w2v_model.save("word2vec.model")
+    money = w2v_model.wv.most_similar("money")
+    print(money)
+
+
+
+def main(data_path, data_name):
+    # word2vec(data_path, data_name, caption_file=annotations)
+    vocab = build_vocab(data_path, data_name, caption_file=annotations, threshold=4)
+    serialize_vocab(vocab, './vocab/%s_vocab.json' % data_name)
+    print("Saved vocabulary file to ", './vocab/%s_vocab.json' % data_name)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', default='data')
+    parser.add_argument('--data_name', default='f30k_precomp',
+                        help='{coco,f30k}_precomp')
+    opt = parser.parse_args()
+    main(opt.data_path, opt.data_name)
